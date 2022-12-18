64a470c38d9edf7a99ed330dc398d1e1174119e9
Your Name <you@example.com>
2022-12-19 00:28:47 +0300

    Задача 2

---
+++ zadach_2/Program.cs
@@ -0,0 +1,13 @@
+﻿    Console.Clear();
+    Console.Write("Введите первое целое число: ");
+    int number1 = int.Parse(Console.ReadLine());
+    Console.Write("Введите второе целое число: ");
+    int number2 = int.Parse(Console.ReadLine());
+    if (number1>number2)
+    {
+      Console.Write($"Наибольшее число из введенных это: {number1}");
+    }
+    else
+    {
+      Console.Write($"Наибольшее число из введенных это: {number2}");
+    }
